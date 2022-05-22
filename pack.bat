bd6f0f0bf870fcde0715a060c899091f18f73222
Nick Kastellanos <nkastellanos@gmail.com>
2022-05-22 21:08:01 +0300

    update pack

--- pack.bat
+++ pack.bat
@@ -2,6 +2,6 @@ dotnet pack Wasm.Dom\Wasm.Dom.csproj /p:Configuration=Release -o bin\publish
 dotnet pack Wasm.XHR\Wasm.XHR.csproj /p:Configuration=Release -o bin\publish
 dotnet pack Wasm.Canvas\Wasm.Canvas.csproj /p:Configuration=Release -o bin\publish
 
-"C:\Program Files (x86)\nuget\nuget.exe" add Wasm.Dom\bin\release\nkast.Wasm.Dom.6.0.0.nupkg -Source "C:\Users\usename\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add Wasm.XHR\bin\release\nkast.Wasm.XHR.6.0.0.nupkg -Source "C:\Users\usename\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add Wasm.Canvas\bin\release\nkast.Wasm.Canvas.6.0.0.nupkg -Source "C:\Users\usename\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add bin\publish\nkast.Wasm.Dom.6.0.0.nupkg -Source "C:\Users\usename\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add bin\publish\nkast.Wasm.XHR.6.0.0.nupkg -Source "C:\Users\usename\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add bin\publish\nkast.Wasm.Canvas.6.0.0.nupkg -Source "C:\Users\usename\.nuget\localPackages"
