1d1c1a7cc7af81fdc1f1f27f4bfc8331109ee172
Nick Kastellanos <nkastellanos@gmail.com>
2023-09-27 10:09:36 +0300

    FixedStructA and Invoke with 10 args

--- Wasm.Dom/WasmJSRuntime.cs
+++ Wasm.Dom/WasmJSRuntime.cs
@@ -58,4 +58,32 @@ namespace nkast.Wasm.Dom
             this.Value9 = value9;
         }
     }
+
+    internal struct FixedStructA<T1, T2, T3, T4, T5, T6, T7, T8, T9, TA>
+    {
+        public readonly T1 Value1;
+        public readonly T2 Value2;
+        public readonly T3 Value3;
+        public readonly T4 Value4;
+        public readonly T5 Value5;
+        public readonly T6 Value6;
+        public readonly T7 Value7;
+        public readonly T8 Value8;
+        public readonly T9 Value9;
+        public readonly TA ValueA;
+
+        public FixedStructA(T1 value1, T2 value2, T3 value3, T4 value4, T5 value5, T6 value6, T7 value7, T8 value8, T9 value9, TA valueA) : this()
+        {
+            this.Value1 = value1;
+            this.Value2 = value2;
+            this.Value3 = value3;
+            this.Value4 = value4;
+            this.Value5 = value5;
+            this.Value6 = value6;
+            this.Value7 = value7;
+            this.Value8 = value8;
+            this.Value9 = value9;
+            this.ValueA = valueA;
+        }
+    }
 }
