f070b2f8c33233bbb6f01f19aa8afccfdf3e1395
Nick Kastellanos <nkastellanos@gmail.com>
2023-10-27 14:27:43 +0300

    update WebGLRenderbufferInternalFormat enums
    added DEPTH_STENCIL, RGBA4, RGB5_A1, RGB565, SRGB8_ALPHA8_EXT

--- Wasm.Canvas/Canvas/WebGL/WebGLRenderbufferInternalFormat.cs
+++ Wasm.Canvas/Canvas/WebGL/WebGLRenderbufferInternalFormat.cs
@@ -6,5 +6,12 @@ namespace nkast.Wasm.Canvas.WebGL
     {
         DEPTH_COMPONENT16   = 0x81A5,
         STENCIL_INDEX8      = 0x8D48,
+        DEPTH_STENCIL       = 0x84F9,
+
+        RGBA4   = 0x8056,
+        RGB5_A1 = 0x8057,
+        RGB565  = 0x8D62,
+        /// <remarks>WebGL1 Extension: EXT_sRGB</remarks>
+        SRGB8_ALPHA8_EXT = 0x8C43,
     }
 }
