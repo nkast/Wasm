479673729950cedc02c0931bef2e8857611e25f6
Nikos Kastellanos <nkastellanos@gmail.com>
2023-10-29 19:28:10 +0200

    Canvas ContextAttributes
    This reverts commit fed6799671656f30bf888d274d627a17ef3c1b9b.

--- Wasm.Canvas/Canvas/Canvas.cs
+++ Wasm.Canvas/Canvas/Canvas.cs
@@ -41,6 +41,7 @@ namespace nkast.Wasm.Canvas
                     return (TContext)(IRenderingContext)_canvasRenderingContext;
 
                 int uid = InvokeRet<int>("nkCanvas.Create2DContext");
+
                 _canvasRenderingContext = new CanvasRenderingContext(this, uid);
 
                 return (TContext)(IRenderingContext)_canvasRenderingContext;
@@ -56,6 +57,57 @@ namespace nkast.Wasm.Canvas
                     return (TContext)(WebGL.IWebGLRenderingContext)_webglRenderingContext;
 
                 int uid = InvokeRet<int>("nkCanvas.CreateWebGLContext");
+
+                _webglRenderingContext = new WebGL.WebGLRenderingContext(this, uid);
+
+                return (TContext)(WebGL.IWebGLRenderingContext)_webglRenderingContext;
+            }
+
+            throw new NotSupportedException();
+        }
+
+        public TContext GetContext<TContext>(ContextAttributes attributes)
+            where TContext : IRenderingContext
+        {
+            if (attributes == null)
+                throw new ArgumentNullException(nameof(attributes));
+
+            if (typeof(TContext) == typeof(ICanvasRenderingContext))
+            {
+                //TODO: implement a Disposed event in IRenderingContext
+                if (_canvasRenderingContext != null && _canvasRenderingContext.IsDisposed)
+                    _canvasRenderingContext = null;
+
+                if (_canvasRenderingContext != null)
+                    return (TContext)(IRenderingContext)_canvasRenderingContext;
+
+                if (attributes.Depth != null
+                ||  attributes.Stencil != null
+                ||  attributes.Antialias != null
+                ||  attributes.PowerPreference != null
+                ||  attributes.PremultipliedAlpha != null
+                ||  attributes.PreserveDrawingBuffer != null
+                ||  attributes.XrCompatible != null)
+                    throw new ArgumentException("attributes are not valid for 2d canvas context.", nameof(attributes));
+
+                int uid = InvokeRet<int, int>("nkCanvas.Create2DContext1", attributes.ToBit());
+                
+                _canvasRenderingContext = new CanvasRenderingContext(this, uid);
+
+                return (TContext)(IRenderingContext)_canvasRenderingContext;
+            }
+
+            if (typeof(TContext) == typeof(WebGL.IWebGLRenderingContext))
+            {
+                //TODO: implement a Disposed event in IRenderingContext
+                if (_webglRenderingContext != null && _webglRenderingContext.IsDisposed)
+                    _webglRenderingContext = null;
+
+                if (_webglRenderingContext != null)
+                    return (TContext)(WebGL.IWebGLRenderingContext)_webglRenderingContext;
+
+                int uid = InvokeRet<int, int>("nkCanvas.CreateWebGLContext1", attributes.ToBit());
+
                 _webglRenderingContext = new WebGL.WebGLRenderingContext(this, uid);
 
                 return (TContext)(WebGL.IWebGLRenderingContext)_webglRenderingContext;
---
+++ Wasm.Canvas/Canvas/ContextAttributes.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nkast.Wasm.Canvas
+{
+    public class ContextAttributes
+    {
+        public enum PowerPreferenceType
+        {
+            Default = 0,
+            HighPerformance = 1,
+            LowPower = 2,
+        }
+
+        public bool? Alpha { get; set; }
+        public bool? Depth { get; set; }
+        public bool? Stencil { get; set; }
+        public bool? Desynchronized { get; set; }
+        public bool? Antialias { get; set; }
+        public PowerPreferenceType? PowerPreference { get; set; }
+        public bool? PremultipliedAlpha { get; set; }
+        public bool? PreserveDrawingBuffer { get; set; }
+        public bool? XrCompatible { get; set; }
+
+
+        public static bool IsNullOrEmpty(ContextAttributes value)
+        {
+            if (value == null)
+                return true;
+
+            if (value.Alpha == null
+            &&  value.Depth == null
+            &&  value.Stencil == null           
+            &&  value.Desynchronized == null
+            &&  value.Antialias == null
+            &&  value.PowerPreference == null
+            &&  value.PremultipliedAlpha == null
+            &&  value.PreserveDrawingBuffer == null
+            &&  value.XrCompatible == null)
+                return true;
+
+            return false;
+        }
+
+        internal int ToBit()
+        {
+            int bits = 0;
+            bits |= ((this.Alpha == null) ? 3 : (this.Alpha == true) ? 1 : 0) <<  0;
+            bits |= ((this.Desynchronized == null) ? 3 : (this.Desynchronized == true) ? 1 : 0) <<  2;
+            bits |= ((this.Depth == null) ? 3 : (this.Depth == true) ? 1 : 0) <<  4;
+            bits |= ((this.Stencil == null) ? 3 : (this.Stencil == true) ? 1 : 0) <<  6;
+            bits |= ((this.Antialias == null) ? 3 : (this.Antialias == true) ? 1 : 0) <<  8;
+            bits |= ((this.PremultipliedAlpha == null) ? 3 : (this.PremultipliedAlpha == true) ? 1 : 0) << 10;
+            bits |= ((this.PreserveDrawingBuffer == null) ? 3 : (this.PreserveDrawingBuffer == true) ? 1 : 0) << 12;
+            bits |= ((this.XrCompatible == null) ? 3 : (this.XrCompatible == true) ? 1 : 0) << 14;
+            bits |= ((this.PowerPreference == null) ? 3 : (int)this.PowerPreference) << 16;
+
+            return bits;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            string seperator = "\n ";
+            sb.Append("{ ");
+            ToStringAppendValue(sb, "alpha", this.Alpha, ref seperator);
+            ToStringAppendValue(sb, "depth", this.Depth, ref seperator);
+            ToStringAppendValue(sb, "stencil", this.Stencil, ref seperator);
+            ToStringAppendValue(sb, "desynchronized", this.Desynchronized, ref seperator);
+            ToStringAppendValue(sb, "antialias", this.Antialias, ref seperator);
+            ToStringAppendValue(sb, "powerPreference", this.PowerPreference, ref seperator);
+            ToStringAppendValue(sb, "premultipliedAlpha", this.PremultipliedAlpha, ref seperator);
+            ToStringAppendValue(sb, "preserveDrawingBuffer", this.PreserveDrawingBuffer, ref seperator);
+            ToStringAppendValue(sb, "xrCompatible", this.XrCompatible, ref seperator);
+            sb.AppendLine("\n }");
+            
+            return sb.ToString();
+        }
+
+        private void ToStringAppendValue(StringBuilder sb, string name, bool? value, ref string seperator)
+        {
+            if (value == null)
+                return;
+
+            sb.Append(seperator); seperator = ",\n ";
+            sb.Append(name); sb.Append(": ");
+            switch (value)
+            {
+                case false:
+                    sb.Append("false");
+                    break;
+                case true:
+                    sb.Append("true");
+                    break;
+
+                default:
+                    throw new InvalidOperationException("");
+            }
+        }
+
+        private void ToStringAppendValue(StringBuilder sb, string name, PowerPreferenceType? value, ref string seperator)
+        {
+            if (value == null)
+                return;
+
+            sb.Append(seperator); seperator = ",\n ";
+            sb.Append(name); sb.Append(": ");
+            switch (value)
+            {
+                case PowerPreferenceType.Default:
+                    sb.Append("\"default\"");
+                    break;
+                case PowerPreferenceType.HighPerformance:
+                    sb.Append("\"high-performance\"");
+                    break;
+                case PowerPreferenceType.LowPower:
+                    sb.Append("\"low-power\"");
+                    break;
+
+                default:
+                    throw new InvalidOperationException("PowerPreferenceType");
+            }
+
+            return;
+        }
+    }
+}
