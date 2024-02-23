7573564839c006cdd3638eb2a40defe0bfcc310e
Nick Kastellanos <nkastellanos@gmail.com>
2024-02-23 06:30:41 +0200

    update Register Nugets

--- pack.bat
+++ pack.bat
@@ -3,7 +3,9 @@ dotnet pack Wasm.XHR\Wasm.XHR.csproj /p:Configuration=Release -o bin\publish
 dotnet pack Wasm.Canvas\Wasm.Canvas.csproj /p:Configuration=Release -o bin\publish
 dotnet pack Wasm.Audio\Wasm.Audio.csproj /p:Configuration=Release -o bin\publish
 
-"C:\Program Files (x86)\nuget\nuget.exe" add bin\publish\nkast.Wasm.Dom.6.0.5.nupkg -Source "C:\Users\usename\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add bin\publish\nkast.Wasm.XHR.6.0.5.nupkg -Source "C:\Users\usename\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add bin\publish\nkast.Wasm.Canvas.6.0.5.nupkg -Source "C:\Users\usename\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add bin\publish\nkast.Wasm.Audio.6.0.5.nupkg -Source "C:\Users\usename\.nuget\localPackages"
+set username=username
+
+"C:\Program Files (x86)\nuget\nuget.exe" add bin\publish\nkast.Wasm.Dom.6.0.5.nupkg -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add bin\publish\nkast.Wasm.XHR.6.0.5.nupkg -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add bin\publish\nkast.Wasm.Canvas.6.0.5.nupkg -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add bin\publish\nkast.Wasm.Audio.6.0.5.nupkg -Source "C:\Users\%username%\.nuget\localPackages"
