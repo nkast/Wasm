75faff6be52aaa530a86caee498db178508c167a
Nikos Kastellanos <nkastellanos@gmail.com>
2024-10-23 20:40:01 +0300

    IWebGLRenderingContext.GetExtensionT()

---
+++ Wasm.Canvas/Canvas/WebGL/WebGLExtension.cs
@@ -0,0 +1,11 @@
+﻿using nkast.Wasm.Dom;
+
+namespace nkast.Wasm.Canvas.WebGL
+{
+    public class WebGLExtension : JSObject
+    {
+        public WebGLExtension(int uid) : base(uid)
+        {
+        }
+    }
+}
---
+++ Wasm.Canvas/Canvas/WebGL/WebGLLoseContextExtension.cs
@@ -0,0 +1,20 @@
+﻿namespace nkast.Wasm.Canvas.WebGL
+{
+    public class WebGLLoseContextExtension : WebGLExtension
+    {
+        public WebGLLoseContextExtension(int uid) : base(uid)
+        {
+        }
+
+        public void LoseContext()
+        {
+            Invoke("nkCanvasLoseContextExtension.LoseContext");
+        }
+
+        public void RestoreContext()
+        {
+            Invoke("nkCanvasLoseContextExtension.RestoreContext");
+        }
+
+    }
+}
