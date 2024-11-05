7f5411f0fdb52f7ad6457bbd564e2bf8b28f5663
Nikos Kastellanos <nkastellanos@gmail.com>
2024-11-05 03:55:50 +0200

    XRWebGLLayer.Framebuffer

--- Wasm.Canvas/Canvas/WebGL/WebGLFramebuffer.cs
+++ Wasm.Canvas/Canvas/WebGL/WebGLFramebuffer.cs
@@ -1,15 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 using nkast.Wasm.Dom;
 
 namespace nkast.Wasm.Canvas.WebGL
 {
     public class WebGLFramebuffer : JSObject
     {
+        static Dictionary<int, WeakReference<JSObject>> _uidMap = new Dictionary<int, WeakReference<JSObject>>();
+
         private IWebGLRenderingContext _glContext;
 
         internal protected WebGLFramebuffer(int uid, IWebGLRenderingContext glContext) : base(uid)
         {
+            _uidMap.Add(Uid, new WeakReference<JSObject>(this, true));
+
             _glContext = (WebGLRenderingContext)glContext;
+        }
+
+        public static WebGLFramebuffer FromUid(int uid)
+        {
+            if (WebGLFramebuffer._uidMap.TryGetValue(uid, out WeakReference<JSObject> jsObjRef))
+                if (jsObjRef.TryGetTarget(out JSObject jsObj))
+                    return (WebGLFramebuffer)jsObj;
+
+            return null;
         }
 
         protected override void Dispose(bool disposing)
@@ -18,6 +32,8 @@ namespace nkast.Wasm.Canvas.WebGL
             {
 
             }
+
+            _uidMap.Remove(Uid);
 
             ((WebGLRenderingContext)_glContext).DeleteFramebuffer(this);
             _glContext = null;
