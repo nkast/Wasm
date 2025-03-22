a4db21e5d3cdf8baa799c624d2f72f6beff601ed
Nikos Kastellanos <nkastellanos@gmail.com>
2025-03-22 13:29:46 +0200

    refactor JSObject

--- Wasm.Dom/Dom/JSObject.Invoke.cs
+++ Wasm.Dom/Dom/JSObject.Invoke.cs
@@ -15,7 +15,7 @@ namespace nkast.Wasm.Dom
 
         protected void Invoke(string identifier)
         {
-            InvokeRet<IJSVoidResult>(identifier);
+            Runtime.InvokeUnmarshalled<int, IJSVoidResult>(identifier, Uid);
         }
 
         protected TRes InvokeRet<TRes>(string identifier)
@@ -25,85 +25,102 @@ namespace nkast.Wasm.Dom
 
         protected void Invoke<T1>(string identifier, T1 arg1)
         {
-            InvokeRet<T1, IJSVoidResult>(identifier, arg1);
+            var args = ValueTuple.Create(arg1, Net7Padding);
+            Runtime.InvokeUnmarshalled<int, ValueTuple<T1, int>, IJSVoidResult>(identifier, Uid, args);
         }
 
         const int Net7Padding = 0;
         protected TRes InvokeRet<T1, TRes>(string identifier, T1 arg1)
         {
-            return Runtime.InvokeUnmarshalled<int, ValueTuple<T1, int>, TRes>(identifier, Uid,
-                ValueTuple.Create(arg1, Net7Padding));
+            var args = ValueTuple.Create(arg1, Net7Padding);
+            return Runtime.InvokeUnmarshalled<int, ValueTuple<T1, int>, TRes>(identifier, Uid, args);
         }
 
         protected void Invoke<T1, T2>(string identifier, T1 arg1, T2 arg2)
         {
-            InvokeRet<T1, T2, IJSVoidResult>(identifier, arg1, arg2);
+            var args = ValueTuple.Create(arg1, arg2);
+            Runtime.InvokeUnmarshalled<int, ValueTuple<T1, T2>, IJSVoidResult>(identifier, Uid, args);
         }
 
         protected TRes InvokeRet<T1, T2, TRes>(string identifier, T1 arg1, T2 arg2)
         {
-            return Runtime.InvokeUnmarshalled<int, ValueTuple<T1, T2>, TRes>(identifier, Uid,
-                ValueTuple.Create(arg1, arg2));
+            var args = ValueTuple.Create(arg1, arg2);
+            return Runtime.InvokeUnmarshalled<int, ValueTuple<T1, T2>, TRes>(identifier, Uid, args);
         }
 
         protected void Invoke<T1, T2, T3>(string identifier, T1 arg1, T2 arg2, T3 arg3)
         {
-            InvokeRet<T1, T2, T3, IJSVoidResult>(identifier, arg1, arg2, arg3);
+            var args = ValueTuple.Create(arg1, arg2, arg3);
+            Runtime.InvokeUnmarshalled<int, ValueTuple<T1, T2, T3>, IJSVoidResult>(identifier, Uid, args);
         }
 
         protected TRes InvokeRet<T1, T2, T3, TRes>(string identifier, T1 arg1, T2 arg2, T3 arg3)
         {
-            return Runtime.InvokeUnmarshalled<int, ValueTuple<T1, T2, T3>, TRes>(identifier, Uid,
-                ValueTuple.Create(arg1, arg2, arg3));
+            var args = ValueTuple.Create(arg1, arg2, arg3);
+            return Runtime.InvokeUnmarshalled<int, ValueTuple<T1, T2, T3>, TRes>(identifier, Uid, args);
         }
 
         protected void Invoke<T1, T2, T3, T4>(string identifier, T1 arg1, T2 arg2, T3 arg3, T4 arg4)
         {
-            InvokeRet<T1, T2, T3, T4, IJSVoidResult>(identifier, arg1, arg2, arg3, arg4);
+            var args = ValueTuple.Create(arg1, arg2, arg3, arg4);
+            Runtime.InvokeUnmarshalled<int, ValueTuple<T1, T2, T3, T4>, IJSVoidResult>(identifier, Uid, args);
         }
 
         protected TRes InvokeRet<T1, T2, T3, T4, TRes>(string identifier, T1 arg1, T2 arg2, T3 arg3, T4 arg4)
         {
-            return Runtime.InvokeUnmarshalled<int, ValueTuple<T1, T2, T3, T4>, TRes>(identifier, Uid,
-                ValueTuple.Create(arg1, arg2, arg3, arg4));
+            var args = ValueTuple.Create(arg1, arg2, arg3, arg4);
+            return Runtime.InvokeUnmarshalled<int, ValueTuple<T1, T2, T3, T4>, TRes>(identifier, Uid, args);
         }
 
         protected void Invoke<T1, T2, T3, T4, T5>(string identifier, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
         {
-            InvokeRet<T1, T2, T3, T4, T5, IJSVoidResult>(identifier, arg1, arg2, arg3, arg4, arg5);
+            var args = ValueTuple.Create(arg1, arg2, arg3, arg4, arg5);
+            Runtime.InvokeUnmarshalled<int, ValueTuple<T1, T2, T3, T4, T5>, IJSVoidResult>(identifier, Uid, args);
         }
 
         protected TRes InvokeRet<T1, T2, T3, T4, T5, TRes>(string identifier, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
         {
-            return Runtime.InvokeUnmarshalled<int, ValueTuple<T1, T2, T3, T4, T5>, TRes>(identifier, Uid,
-                ValueTuple.Create(arg1, arg2, arg3, arg4, arg5));
+            var args = ValueTuple.Create(arg1, arg2, arg3, arg4, arg5);
+            return Runtime.InvokeUnmarshalled<int, ValueTuple<T1, T2, T3, T4, T5>, TRes>(identifier, Uid, args);
         }
 
         protected void Invoke<T1, T2, T3, T4, T5, T6>(string identifier, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6)
         {
-            InvokeRet<T1, T2, T3, T4, T5, T6, IJSVoidResult>(identifier, arg1, arg2, arg3, arg4, arg5, arg6);
+            var args = ValueTuple.Create(arg1, arg2, arg3, arg4, arg5, arg6);
+            Runtime.InvokeUnmarshalled<int, ValueTuple<T1, T2, T3, T4, T5, T6>, IJSVoidResult>(identifier, Uid, args);
         }
 
         protected TRes InvokeRet<T1, T2, T3, T4, T5, T6, TRes>(string identifier, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6)
         {
-            return Runtime.InvokeUnmarshalled<int, ValueTuple<T1, T2, T3, T4, T5, T6>, TRes>(identifier, Uid,
-                ValueTuple.Create(arg1, arg2, arg3, arg4, arg5, arg6));
+            var args = ValueTuple.Create(arg1, arg2, arg3, arg4, arg5, arg6);
+            return Runtime.InvokeUnmarshalled<int, ValueTuple<T1, T2, T3, T4, T5, T6>, TRes>(identifier, Uid, args);
         }
 
         protected void Invoke<T1, T2, T3, T4, T5, T6, T7>(string identifier, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7)
         {
-            InvokeRet<T1, T2, T3, T4, T5, T6, T7, IJSVoidResult>(identifier, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+            var args = ValueTuple.Create(arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+            Runtime.InvokeUnmarshalled<int, ValueTuple<T1, T2, T3, T4, T5, T6, T7>, IJSVoidResult>(identifier, Uid, args);
         }
 
         protected TRes InvokeRet<T1, T2, T3, T4, T5, T6, T7, TRes>(string identifier, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7)
         {
-            return Runtime.InvokeUnmarshalled<int, ValueTuple<T1, T2, T3, T4, T5, T6, T7>, TRes>(identifier, Uid,
-                ValueTuple.Create(arg1, arg2, arg3, arg4, arg5, arg6, arg7));
+            var args = ValueTuple.Create(arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+            return Runtime.InvokeUnmarshalled<int, ValueTuple<T1, T2, T3, T4, T5, T6, T7>, TRes>(identifier, Uid, args);
         }
 
         protected void Invoke<T1, T2, T3, T4, T5, T6, T7, T8>(string identifier, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8)
         {
-            InvokeRet<T1, T2, T3, T4, T5, T6, T7, T8, IJSVoidResult>(identifier, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
+            var args = new FixedStruct8<T1, T2, T3, T4, T5, T6, T7, T8>(
+                value1: arg1,
+                value2: arg2,
+                value3: arg3,
+                value4: arg4,
+                value5: arg5,
+                value6: arg6,
+                value7: arg7,
+                value8: arg8
+            );
+            Runtime.InvokeUnmarshalled<int, FixedStruct8<T1, T2, T3, T4, T5, T6, T7, T8>, IJSVoidResult>(identifier, Uid, args);
         }
 
         protected TRes InvokeRet<T1, T2, T3, T4, T5, T6, T7, T8, TRes>(string identifier, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8)
@@ -123,7 +140,8 @@ namespace nkast.Wasm.Dom
 
         protected void Invoke<T1, T2, T3, T4, T5, T6, T7, T8, T9>(string identifier, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9)
         {
-            InvokeRet<T1, T2, T3, T4, T5, T6, T7, T8, T9, IJSVoidResult>(identifier, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
+            var args = new FixedStruct9<T1, T2, T3, T4, T5, T6, T7, T8, T9>(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
+            Runtime.InvokeUnmarshalled<int, FixedStruct9<T1, T2, T3, T4, T5, T6, T7, T8, T9>, IJSVoidResult>(identifier, Uid, args);
         }
 
         protected TRes InvokeRet<T1, T2, T3, T4, T5, T6, T7, T8, T9, TRes>(string identifier, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9)
@@ -134,7 +152,8 @@ namespace nkast.Wasm.Dom
 
         protected void Invoke<T1, T2, T3, T4, T5, T6, T7, T8, T9, TA>(string identifier, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, TA argA)
         {
-            InvokeRet<T1, T2, T3, T4, T5, T6, T7, T8, T9, TA, IJSVoidResult>(identifier, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, argA);
+            var args = new FixedStructA<T1, T2, T3, T4, T5, T6, T7, T8, T9, TA>(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, argA);
+            Runtime.InvokeUnmarshalled<int, FixedStructA<T1, T2, T3, T4, T5, T6, T7, T8, T9, TA>, IJSVoidResult>(identifier, Uid, args);
         }
 
         protected TRes InvokeRet<T1, T2, T3, T4, T5, T6, T7, T8, T9, TA, TRes>(string identifier, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, TA argA)
