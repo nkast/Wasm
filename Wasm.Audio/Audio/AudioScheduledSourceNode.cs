3a8e0b9a500dc689060c4845200236b3a5b37ec9
Nikos Kastellanos <nkastellanos@gmail.com>
2025-06-17 09:05:47 +0300

    update Audio usings

--- Wasm.Audio/Audio/AudioScheduledSourceNode.cs
+++ Wasm.Audio/Audio/AudioScheduledSourceNode.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.JSInterop;
-using nkast.Wasm.Dom;
+using nkast.Wasm.JSInterop;
 
 namespace nkast.Wasm.Audio
 {
--- Wasm.Audio/Audio/MediaElementAudioSourceNode.cs
+++ Wasm.Audio/Audio/MediaElementAudioSourceNode.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using nkast.Wasm.Dom;
+using nkast.Wasm.JSInterop;
 
 namespace nkast.Wasm.Audio
 {
